d0f403a8010427a5e4964d8bdd319a78ce8870c0
lojdis <113169810+lojdis@users.noreply.github.com>
2023-01-12 10:36:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace simpsnovi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] simpsonovi = { "Homer", "Marge", "Bart", "Lisa", "Magie" };
+            Array.Sort(simpsonovi);
+            foreach (string serazeno in simpsonovi)
+                Console.WriteLine("{0}", serazeno);
+            Console.ReadKey();
+        }
+    }
+}
